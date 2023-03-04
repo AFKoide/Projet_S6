2f9291136ace5f75de1ff2734fce7b83e44849a4
Gerier Kantique <chose.neant@gmail.com>
2023-03-04 19:09:43 +0100

    Update Configuration Appareil.py

--- Configuration Appareil.py
+++ Configuration Appareil.py
@@ -1,19 +1,8 @@
-# Suit la ligne noire
-# Si se pert, tourne sur lui-même jusqu'à la trouver
-# Si rencontre un obstacle, fait demi-tour
-# Si les trois capteurs activés, s'arrete
-# Une LED clignote pour indiquer que ca fonctionne
-# Appuie sur un bouton pour lancer programme
-# Avant lancement programme, LED indique si capteur fonctionne
+# Utilisé pour configurer la machine et prendre en main le code
 
 from PicoAutonomousRobotics import KitronikPicoRobotBuggy
 from time import sleep_us, sleep, sleep_ms
 buggy = KitronikPicoRobotBuggy()
-
-buggy.setBrightness(50)
-buggy.setMeasurementsTo("cm")
-buggy.setLFDarkValue(40000)
-buggy.setLFLightValue(15000)
 
 etat = 0
 
@@ -27,17 +16,17 @@ def ButtonIRQHandler(pin):
 buggy.button.irq(trigger=machine.Pin.IRQ_RISING, handler =  ButtonIRQHandler)
 
 while True:
-    if (etat == 0):
+    if (etat == 0): # ATTENTE POUR CONFIGURATION
         buggy.setLED(2,buggy.GREEN)
         buggy.show()
         pass
-    elif (etat == 1):
+    elif (etat == 1):  # DETERMINER LES SEUILS
         buggy.setLED(2,buggy.YELLOW)
         print ("L" , buggy.getRawLFValue("l"), " R", buggy.getRawLFValue("r"), " C",buggy.getRawLFValue("c"))
         buggy.show()
         sleep(3)
         pass
-    elif (etat == 2):
+    elif (etat == 2): # VERIFIER LA DISTANCE
         buggy.setLED(2,buggy.BLUE)
         print(buggy.getDistance("f"))
         print(buggy.getDistance("r"))
