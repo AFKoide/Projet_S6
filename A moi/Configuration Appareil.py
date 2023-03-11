00af6c1e3c7bd96245a95fd7791a0bb0e0d47a7c
Gerier Kantique <chose.neant@gmail.com>
2023-03-12 00:00:43 +0100

    Reorganisation

---
+++ A moi/Configuration Appareil.py
@@ -0,0 +1,57 @@
+# Utilisé pour configurer la machine et prendre en main le code
+
+from PicoAutonomousRobotics import KitronikPicoRobotBuggy
+from time import sleep_us, sleep, sleep_ms
+buggy = KitronikPicoRobotBuggy()
+
+etat = 0
+
+def ButtonIRQHandler(pin):
+    global etat
+    etat=etat+1
+    etat=etat%3
+    buggy.beepHorn()
+    sleep(1)
+
+buggy.button.irq(trigger=machine.Pin.IRQ_RISING, handler =  ButtonIRQHandler)
+
+while True:
+    if (etat == 0): # ATTENTE POUR CONFIGURATION
+        buggy.setLED(2,buggy.GREEN)
+        buggy.show()
+        pass
+    elif (etat == 1):  # DETERMINER LES SEUILS
+        buggy.setLED(2,buggy.YELLOW)
+        print ("L" , buggy.getRawLFValue("l"), " R", buggy.getRawLFValue("r"), " C",buggy.getRawLFValue("c"))
+        buggy.show()
+        sleep(3)
+        pass
+    elif (etat == 2): # VERIFIER LA DISTANCE
+        buggy.setLED(2,buggy.BLUE)
+        print(buggy.getDistance("f"))
+        print(buggy.getDistance("r"))
+        buggy.show()
+        sleep(3)
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
