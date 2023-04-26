f80dda62f9259e8a69b848125cb5e6ead83260d9
Mphelps30 <127611256+Mphelps30@users.noreply.github.com>
2023-04-26 16:11:27 +0200

    UPDATE

--- Dylan/v1.py
+++ Dylan/v1.py
@@ -1,33 +1,60 @@
+# Faire Avancer le Robot
+
 from PicoAutonomousRobotics import KitronikPicoRobotBuggy
-from time import sleep_us, sleep, sleep_ms
-from Fonction import *
-
 buggy = KitronikPicoRobotBuggy()
-
-buggy.setBrightness(50)
-buggy.setMeasurementsTo("cm")
-buggy.setLFDarkValue(40000)
-buggy.setLFLightValue(15000)
+from time import sleep_us, sleep, sleep_ms
 
 etat = 0
 
 def ButtonIRQHandler(pin):
     global etat
-    etat ^= 1
-
-    if etat is False:
-        buggy.motorOn("l","f",0);buggy.motorOn("r","f",0)
+    etat +=1
+    etat = etat%2
 buggy.button.irq(trigger=machine.Pin.IRQ_RISING, handler =  ButtonIRQHandler)
 
-i = 0
+buggy.setLED(0,buggy.PURPLE)
+buggy.setLED(1,buggy.PURPLE)
+buggy.setLED(2,buggy.PURPLE)
+buggy.setLED(3,buggy.PURPLE)
+buggy.show()
 
-while True:
-    if etat is True:
-        buggy.motorOn("l","f",v_gauche);buggy.motorOn("r","f",v_droit)
+noir = 40000
+blanc = 20000
 
-    gauche[i] = buggy.isLFSensorLight("l")
-    droite[i] = buggy.isLFSensorLight("r")
-    centre[i] = buggy.isLFSensorLight("c")
-    devant[i] = buggy.getDistance("f")
+vgauche = 75
+vdroite = 75
 
-    if 
+while True:    
+    gauche = buggy.getRawLFValue("l")
+    droite = buggy.getRawLFValue("r")
+    centre = buggy.getRawLFValue("c")
+    devant = buggy.getDistance("f")
+    
+    print("\netat=",etat)
+    print("gauche=",gauche)
+    print("droite=",droite)
+    print("centre=",centre)
+    
+    print("vitesse gauche=",vgauche)
+    print("vitesse droite=",vdroite)
+    
+    if gauche >= noir:
+        if vdroite<=100: vdroite += 1
+        if vgauche>=70: vgauche -= 1
+    elif droite >= noir:
+        if vdroite>=70: vdroite -= 1
+        if vgauche<=100: vgauche += 1
+    elif gauche <=blanc and noir <= blanc:
+        vgauche = 75
+        vdroite = 75
+
+    if etat == 1:
+        buggy.motorOn("l","f",vgauche)
+        buggy.motorOn("r","f",vdroite)
+        
+        
+    sleep_ms(20)
+    buggy.motorOn("l","f",0)
+    buggy.motorOn("r","f",0)
+    sleep(0.2)
+
